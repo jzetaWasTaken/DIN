54392add419afa4b8e19bc170cecc916766552d9
Jon Zaballa <jzaballazarzosa@gmail.com>
2018-01-09 09:49:33 +0100

    EJBLocal y algunas cosas más

---
+++ GestionBancariaServerSide/src/java/gestionbancariaserver/ejb/BankingEJBLocal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionbancariaserver.ejb;
+
+import gestionbancariaserver.entity.Account;
+import gestionbancariaserver.entity.Transaction;
+import gestionbancariaserver.exceptions.ReadException;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author ubuntu
+ */
+@Local
+public interface BankingEJBLocal {
+        
+        public List<Account> findAccountsByCustomerId(Long id) throws ReadException;
+        public List<Transaction> findTransactionsByAccount(Long id) throws ReadException;
+        public List<Transaction> findDepositsByAccount(Long id) throws ReadException;
+        public List<Transaction> findPaymentsByAccount(Long id) throws ReadException;
+        public List<Transaction> findTransfersByAccount(Long id) throws ReadException;
+        // TODO think about how to do login (see Credential and Customer entity)
+    
+}
